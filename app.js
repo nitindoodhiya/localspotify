add034f4b96c3e2c84bd74671f128af81ab09175
nitindoodhiya@gmail.com <n14#t20D4>
2021-07-01 18:35:00 +0530

    coded cleaned

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { send } = require('process');
 
 var app = express();
 
@@ -14,28 +16,32 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/downloadapp', async function(req, res, next) {
+    res.send(JSON.parse(req.body.data));
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,64 +2,94 @@ var express = require('express');
 var router = express.Router();
 var Downloader = require("./downloader");
 const fs = require('fs');
-
+const AUDIO_PATH = "./public/audios/";
 var dl = new Downloader();
 var i = 0;
 const axios = require('axios')
 const cheerio = require('cheerio')
-const fsLibrary  = require('fs');
+const fsLibrary = require('fs');
 const { stringify } = require('querystring');
 const { Client } = require("youtubei");
 const youtube = new Client();
 var path = require('path');
-
+router.use(express.json());
 /* GET home page. */
 
-router.get('/',async function (req, res, next) {
-  res.render('home');
+router.get('/', async function (req, res, next) {
+    res.render('home');
 });
 
-router.get('/:spotifyID',async function (req, res, next) {
-  axios.get('https://open.spotify.com/playlist/'+req.params.spotifyID).then((response) => {
-          const $ = cheerio.load(response.data);
-          const urlElems = $("script");
-          var r = urlElems[5].children[0].data;
-        
-          r = r.substring(39,r.length-5);
-          r = JSON.parse(r);
-          
-          console.log(r.tracks.items[3].track.artists[0].name);
-          res.render('index',{playlist:r});
+// req.body {
+//     name: "name"
+// }  suppose to be 
+
+router.post('/generatelink', async function (req, res, next) {
+    let data = JSON.parse(req.body.data);
+    let song_name = data.name;
+    data.artists.forEach(e => {
+        song_name += " " + e.name;
+    });
+    if (fs.existsSync(path.resolve(AUDIO_PATH + song_name + '.mp3'))) {
+        res.send({
+            "status": 200,
+            "message": "Song Available now"
         });
+    } else {
+        const videos = await youtube.search(song_name, {
+            type: "video", // video | playlist | channel | all
+        });
+        console.log(videos[0].id);
+        await dl.getMP3({ videoId: videos[0].id, name: song_name + '.mp3' }, function (err, response) {
+            i++;
+            console.log('fdg');
+            if (err) {
+                console.log(err);
+                res.send({
+                    "status": 500,
+                    "message": "Mamla Internal Hai"
+                });
+            }
+            else {
+                console.log("Song " + i + " was downloaded: " + response.file);
+                res.send({
+                    "status": 200,
+                    "message": "Song Available now"
+                });
+            }
+        });
+    }
+});
+router.get('/downloadsong/:song_name', async function (req, res, next) {
+    let song_name = req.params.song_name;
+    if (fs.existsSync(path.resolve(AUDIO_PATH + song_name + '.mp3'))) {
+        res.download(path.resolve(AUDIO_PATH + song_name + '.mp3'))
+    } else {
+        res.send("NOT AVAILABLE")
+    }
+});
+router.get('/errorsong/:song_name', async function (req, res, next) {
+    let song_name = req.params.song_name;
+    res.send("SONG COULD NOT BE DOWNLOADED FOR SOME REASONS: "+ song_name);
 });
 
-router.get('/download/:name/:artists', async function (req,res,next) {
-  console.log(req.params.name);
-  var str = req.params.name + ' '+ req.params.artists;
-  // var mp3file = new File(path.resolve("./public/audios/"+req.params.name + '.mp3'));
-  if(fs.existsSync(path.resolve("./public/audios/"+req.params.name + '.mp3'))) {
-    res.download(path.resolve("./public/audios/"+req.params.name + '.mp3'));
+router.get('/:spotifyID', async function (req, res, next) {
+    if (req.params.spotifyID !== "favicon.ico")
+        axios.get('https://open.spotify.com/playlist/' + req.params.spotifyID).then((response) => {
+            const $ = cheerio.load(response.data);
+            const urlElems = $("script");
+            var r = urlElems[5].children[0].data;
 
-  } else 
-  {const videos = await youtube.search(str, {
-		type: "video", // video | playlist | channel | all
-	});
-  console.log(videos[0].id);
-  await dl.getMP3({videoId: videos[0].id, name: req.params.name+'.mp3'}, function(err,response){
-    i++;
-    console.log('fdg');
-    if(err)
-        throw err;
-    else{
+            r = r.substring(39, r.length - 5);
+            r = JSON.parse(r);
 
-        console.log("Song "+ i + " was downloaded: " + response.file);
+            console.log(r.tracks.items[3].track.artists[0].name);
+            res.render('index', { Jsonplaylist: JSON.stringify(r), playlist: r, items: JSON.stringify(r.tracks.items) });
+        }).catch((error) => {
+            console.log(error);
+            res.send('cannot find the song mp3')
+        });
+    else res.send("not found");
+});
 
 
-        res.download(path.resolve("./public/audios/"+req.params.name + '.mp3'));
-        // res.render('download',{link:""})
-      }
-  });
-}
-
-})
 module.exports = router;
